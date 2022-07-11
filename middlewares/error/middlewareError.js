654b0dea1d224a5cda1e16f54329a73d49b59caa
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-07-11 20:38:06 -0300

    🐱 feat: recriando o middleware de erro

--- middlewares/error/middlewareError.js
+++ middlewares/error/middlewareError.js
@@ -1,21 +1,10 @@
-const statusByErrorCode = {
-  'string.min': 422,
-  'any.required': 400,
-    'number.min': 422,
-    'number.positive': 422,
-    invalidData: 400,
-    notFound: 404,
-    alreadyExists: 409,
-  };
-  module.exports = (err, _req, res, _next) => {
-    if (err.isJoi) {
-      const status = statusByErrorCode[err.details[0].type] || 500;
-      return res.status(status).json({ message: err.details[0].message });
-    }
-    if (err.code) {
-      const status = statusByErrorCode[err.code] || 500;
-      return res.status(status).json(err);
-    }
-    console.error(err);
-    return res.status(500).json({ message: 'Internal server error' });
-  };
+const middlewareError = (err, _req, res, _next) => {
+  const { name, message } = err;
+  switch (name) {
+    case 'ValidationError': res.status(400).json({ message }); break;
+    case 'NotFoundError': res.status(404).json({ message }); break;
+    default: console.warn(err); res.sendStatus(500);
+  }
+};
+
+module.exports = middlewareError;
